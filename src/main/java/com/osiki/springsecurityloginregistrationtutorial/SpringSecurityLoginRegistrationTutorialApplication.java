c9bdc8e68a4c460f9f6c472ed7235671a8059007
Danny4life <osiki.segun@gmail.com>
2023-10-30 19:27:14 +0100

    innitial commit

---
+++ src/main/java/com/osiki/springsecurityloginregistrationtutorial/SpringSecurityLoginRegistrationTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.osiki.springsecurityloginregistrationtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityLoginRegistrationTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityLoginRegistrationTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/osiki/springsecurityloginregistrationtutorial/SpringSecurityLoginRegistrationTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osiki.springsecurityloginregistrationtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLoginRegistrationTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
